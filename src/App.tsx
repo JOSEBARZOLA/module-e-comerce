c09b466c9b31a57ea9edce0dc6fccd61458652b8
JOSEBARZOLA <jose_barzola89@hotmail.com>
2025-07-31 02:57:38 -0300

    bulma-variables-main funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,72 @@
-
+import "./assets/sass/_main.scss";
 
 function App() {
-
   return (
     <>
-      <div>
-        <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      </div>
-      
+      <section className="hero is-primary is-fullheight">
+        <div className="hero-head">
+          <header className="navbar">
+            <div className="container">
+              <div className="navbar-brand">
+                <a className="navbar-item">
+                  <p className="title">LOGO</p>
+                </a>
+                <span className="navbar-burger" data-target="navbarMenuHeroC">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </span>
+              </div>
+              <div id="navbarMenuHeroC" className="navbar-menu">
+                <div className="navbar-end">
+                  <a className="navbar-item"> Home </a>
+                  <a className="navbar-item"> Examples </a>
+                  <a className="navbar-item"> Documentation </a>
+                  <span className="navbar-item">
+                    <a className="button is-light">
+                      <span className="icon">
+                        <i className="fab fa-github"></i>
+                      </span>
+                      <span>Download</span>
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </header>
+        </div>
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <p className="title">Title</p>
+            <p className="subtitle">Subtitle</p>
+          </div>
+        </div>
+        <div className="hero-foot">
+          <nav className="tabs is-boxed is-fullwidth">
+            <div className="container">
+              <ul>
+                <li className="navbar-item">
+                  <a>Overview</a>
+                </li>
+                <li className="navbar-item">
+                  <a>Modifiers</a>
+                </li>
+                <li className="navbar-item">
+                  <a>Grid</a>
+                </li>
+                <li className="navbar-item">
+                  <a>Elements</a>
+                </li>
+                <li className="navbar-item">
+                  <a>Components</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </section>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
