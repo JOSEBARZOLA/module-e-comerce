e089b7497cb3335d948da75a5e5d90a2bb7963ef
JOSEBARZOLA <jose_barzola89@hotmail.com>
2025-08-01 19:28:04 -0300

    createComponent cardsSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar";
 import MarqueeBanner from "./components/marqueeBanner";
+import CardsSection from "./components/cardsSection";
 
 function App() {
   return (
     <>
       <Navbar/>
-      <MarqueeBanner />
+      <MarqueeBanner/>
+      <CardsSection/>
     </>
   );
 }
